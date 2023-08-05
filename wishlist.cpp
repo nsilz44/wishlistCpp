77cb78c8dba78894bf7e8e25c845f6cc7e031703
Nsilz44 <n_stbss@hotmail.com>
2023-08-06 00:02:43 +0100

    added option to view whole list

--- wishlist.cpp
+++ wishlist.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+
 using namespace std;
 
 #define LIST "list.txt"
@@ -11,14 +12,28 @@ void PrintList(string filename);
 int main () {
     PrintList(LIST);
     PrintOptions();
+    string option;
+    cin >> option;
+    cout << '\n';
+    if(option.size() > 1){
+        char optionLetter = option[1];
+        switch (optionLetter)
+        {
+        case 'v': case 'V':
+            PrintList(LIST);
+            break;
+        default:
+            break;
+        }
+    }
 }
 
 // prints whole list
 void PrintList(string filename){
     ifstream list(filename);
+    cout << "current wishlist" << '\n';
     if(list.is_open()){
         string tp;
-        cout << tp << "\n";
         while (getline (list, tp)) {
     // Output the text from the file
         cout << tp << '\n';
@@ -26,6 +41,7 @@ void PrintList(string filename){
         list.close();
     }
 }
+
 void PrintOptions(){
     cout << "To view current list type '-v'" << '\n';
     cout << "To add item type '-a [ITEM]'" << '\n';
